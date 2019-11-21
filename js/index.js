ccb447659a98f942317464c1c5d622980cfa2af3
Mozillex <Mozillex@users.noreply.github.com>
2019-11-21 15:55:41 -0500

    Change from Atom to VSCode

--- js/index.js
+++ js/index.js
@@ -9,21 +9,21 @@ let entry = [];
 let result = 0;
 let decimal = null;
 
-const operatorKeys = document.getElementsByClassName('key operator');
-const numKeys = document.getElementsByClassName('key number');
-let output = get('output');
-let formula = get('formula');
+const operatorKeys = document.getElementsByClassName("key operator");
+const numKeys = document.getElementsByClassName("key number");
+let output = get("output");
+let formula = get("formula");
 
 function clearIt(what) {
-	if (what === 'all') {
+	if (what === "all") {
 		equation = [];
 		output.textContent = 0;
 		formula.textContent = "I'm counting on you";
 		result = 0;
 	}
-	if (what === 'entry') {
+	if (what === "entry") {
 		output.textContent = formula.length === 0 ? 0 : result;
-		formula.textContent = equation.length === 0 ? 'waiting...' : equation;
+		formula.textContent = equation.length === 0 ? "waiting..." : equation;
 	}
 	entry = [];
 	decimal = undefined;
@@ -32,37 +32,34 @@ function clearIt(what) {
 //set up the number keys (including decimal)
 
 for (let i in numKeys) {
-	numKeys[i].onclick = function() {
-		if (this.value === '.' && decimal === true) {
+	numKeys[i].onclick = function () {
+		if (this.value === "." && decimal === true) {
 			return;
 		}
 
-		if (this.value === '.' && decimal === null) {
+		if (this.value === "." && decimal === null) {
 			if (entry.length === 0) entry.push(0);
 			decimal = true;
 		}
 
 		if (this.value === 0 && equation.length === 0) return;
 		if (this.value === 0 && entry.length === 0) return;
-		if (this.value === '.' && entry.length === 0) entry.push(0);
+		if (this.value === "." && entry.length === 0) entry.push(0);
 
 		entry.push(this.value);
-		output.textContent = entry.join('').slice(0, 10);
-		formula.textContent = (equation.join(' ') + ' ' + entry.join('')).slice(
-			0,
-			60
-		);
+		output.textContent = entry.join("").slice(0, 10);
+		formula.textContent = (equation.join(" ") + " " + entry.join("")).slice(0, 60);
 	};
 }
 // set up the operator (+ - / *) keys
 
 for (let i in operatorKeys) {
-	operatorKeys[i].onclick = function() {
-		if (entry.length - 1 === '.') {
+	operatorKeys[i].onclick = function () {
+		if (entry.length - 1 === ".") {
 			entry.pop(); //if the last entry was a '.' get rid of it
 		}
 		if (entry.length > 0) {
-			equation.push(Number(entry.join('')));
+			equation.push(Number(entry.join("")));
 		} else if (entry.length === 0) {
 			if (equation.length % 2 === 0) equation.pop();
 			else if (equation.length < 2)
@@ -71,7 +68,7 @@ for (let i in operatorKeys) {
 		}
 
 		equation.push(this.value);
-		formula.textContent = equation.join(' ').slice(0, 60);
+		formula.textContent = equation.join(" ").slice(0, 60);
 		entry = [];
 		decimal = null;
 	};
@@ -86,12 +83,12 @@ function equals() {
 	if (entry.length === 0 && isAnOperator.test(lastEntry) === true) {
 		equation.pop();
 	}
-	if (entry.length - 1 === '.') {
+	if (entry.length - 1 === ".") {
 		entry.pop();
 	} else if (equation.length < 2) {
-		equation.push(entry.join(''));
-		output.textContent = 'error';
-		formula.textContent = equation.join(' '); //show user the faulty formula
+		equation.push(entry.join(""));
+		output.textContent = "error";
+		formula.textContent = equation.join(" "); //show user the faulty formula
 
 		equation = [];
 		entry = [];
@@ -100,13 +97,13 @@ function equals() {
 		return;
 	}
 	if (entry.length === 1) equation.push(Number(entry));
-	if (entry.length > 1) equation.push(Number(entry.join('')));
+	if (entry.length > 1) equation.push(Number(entry.join("")));
 
-	let final = equation.join(' ').toString();
+	let final = equation.join(" ").toString();
 	result = eval(final);
 
 	output.textContent = result.toString().slice(0, 10);
-	formula.textContent = (equation.join(' ') + ' = ' + result)
+	formula.textContent = (equation.join(" ") + " = " + result)
 		.toString()
 		.slice(0, 50);
 	equation = [];
